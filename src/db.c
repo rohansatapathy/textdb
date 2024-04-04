6650474a4a9c1e54e586ee3f8d9fb44e4b5d5418
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2024-04-03 20:00:27 -0400

    Refactor searching into db_find

--- src/db.c
+++ src/db.c
@@ -19,29 +19,40 @@ void db_resize(struct db* db, int new_capacity) {
     db->capacity = new_capacity;
 }
 
+/*
+ * Return a pointer to the dbitem with the given key. Returns NULL if the key
+ * couldn't be found.
+ */
+struct dbitem* db_find(struct db* db, char key[]) {
+    bool key_found;
+    for (struct dbitem* i = db->items; i < db->items + db->size; i++) {
+        if (strcmp(key, i->key) == 0) {
+            return i;
+        }
+    }
+    return NULL;
+}
+
 void db_put(struct db* db, char key[], char value[]) {
     char* v = malloc(sizeof(char) * strlen(value));
     strncpy(v, value, strlen(value));
 
     // Check if the key already exists
-    bool key_found;
-    for (int i = 0; i < db->size; i++) {
-        key_found = strcmp(key, db->items[i].key) == 0;
-        if (key_found) {
-            free(db->items[i].value);
-            db->items[i].value = v;
-            return;
-        }
+    struct dbitem* item = db_find(db, key);
+    if (item != NULL) {
+        free(item->value);
+        item->value = v;
+        return;
     }
 
     // Key not found, so create new dbitem
     char* k = malloc(sizeof(char) * strlen(key));
     k = strncpy(k, key, strlen(key));
-    struct dbitem item = {.key = k, .value = v};
+    struct dbitem new_item = {.key = k, .value = v};
     if (db->size == db->capacity) {
         db_resize(db, 2 * db->size);
     }
-    db->items[db->size] = item;
+    db->items[db->size] = new_item;
     db->size++;
 }
 
@@ -77,7 +88,14 @@ struct db* db_init(FILE* fp) {
             if (*i == ' ') {
                 // Replace space with sentinel to separate key and value
                 *i = '\0';
-                value = i + 1;
+                value = ++i;
+
+                // Replace trailing newline with sentinel
+                while (*i != '\n' && *i != '\0') {
+                    i++;
+                }
+                *i = '\0';
+
                 db_put(db, key, value);
             }
         }
